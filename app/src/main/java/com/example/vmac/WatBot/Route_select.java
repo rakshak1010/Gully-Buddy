599eb42ae33022cd66949bb4274eac5fae8b7a8a
Rakshak <rakshak.malhotra48@gmail.com>
2019-12-01 03:09:42 +0530

    Current Location Latitude longitude added

--- app/src/main/java/com/example/vmac/WatBot/Route_select.java
+++ app/src/main/java/com/example/vmac/WatBot/Route_select.java
@@ -1,10 +1,19 @@
 package com.example.vmac.WatBot;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,11 +21,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
 
-public class Route_select extends AppCompatActivity implements OnMapReadyCallback{
+
+public class Route_select extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap mapAPI;
     SupportMapFragment mapFragment;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,82 @@ public class Route_select extends AppCompatActivity implements OnMapReadyCallbac
 
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+                    LatLng latlng = new LatLng(latitude, longitude);
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList =  geocoder.getFromLocation(latitude, longitude, 1);
+                        String str = addressList.get(0).getLocality();
+                        str += addressList.get(0).getLocality();
+                        mapAPI.addMarker(new MarkerOptions().position(latlng).title(str));
+                        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,17f));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+                }
+            });
+        }else if (locationManager.isProviderEnabled((LocationManager.GPS_PROVIDER))){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+                    LatLng latlng = new LatLng(latitude, longitude);
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList =  geocoder.getFromLocation(latitude, longitude, 1);
+                        String str = addressList.get(0).getLocality();
+                        str += addressList.get(0).getLocality();
+                        mapAPI.addMarker(new MarkerOptions().position(latlng).title(str));
+                        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,17f));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+                }
+            });
+
+        }
 
         final Button button = (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
@@ -42,11 +131,11 @@ public class Route_select extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
         mapAPI = googleMap;
-        LatLng current = new LatLng(23.2672493,77.3680164);
-
-        mapAPI.addMarker(new MarkerOptions().position(current));
-
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(current,17f));
+//        LatLng current = new LatLng(23.2672493,77.3680164);
+//
+//        mapAPI.addMarker(new MarkerOptions().position(current));
+//
+//        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(current,17f));
 //        mapAPI.setMapType(mapAPI.MAP_TYPE_SATELLITE);
 
     }
